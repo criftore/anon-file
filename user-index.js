57f617095afba1dddd5a836773ff244c6aedb969
Criftore <taufikboy392@gmail.com>
2026-02-10 06:13:21 +0700

    update index.html and home.html

--- user-index.js
+++ user-index.js
@@ -22,24 +22,50 @@ function Index() {
   }, 1000);
 }
 
+function unikId() {
+  const karakter =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+  for (let i = 0; i < 6; i++) {
+    id += karakter.charAt(Math.floor(Math.random() * karakter.length));
+  }
+  return id;
+}
+
 function prosesLogin() {
-  let nama = document.getElementById("username").value;
-  if (nama === "") {
+  let username = document.getElementById("username").value;
+  if (username === "") {
     notifPopup("Isi dulu usernamenya Bosku!");
     return false;
   } else {
-    localStorage.setItem("user_aktif", nama);
-    notifPopup("Halo " + nama + " Menghubungkan...");
+    const idBaru = unikId();
+    localStorage.setItem("user_aktif", username);
+    localStorage.setItem("user_id", idBaru);
+
+    notifPopup("Halo " + username + " Menghubungkan...");
     fade();
+
     setTimeout(() => {
       window.location.href = "home.html";
     }, 1000);
     return false;
   }
 }
+
+const btnLogin = document.getElementById("btn-login");
+if (btnLogin) {
+  btnLogin.onclick = function (event) {
+    event.preventDefault();
+    prosesLogin();
+  };
+}
+
 function salam() {
   let namaUser = localStorage.getItem("user_aktif");
   let elementSalam = document.getElementById("Salam");
+  let idUser = localStorage.getItem("user_id");
+  let elementId = document.getElementById("uID");
+
   if (elementSalam) {
     if (namaUser) {
       elementSalam.innerText = "Halo, " + namaUser + "!";
@@ -48,10 +74,48 @@ function salam() {
       Index();
     }
   }
+  if (elementId) {
+    elementId.innerText = "#" + idUser;
+  }
 }
 function prosesLogout() {
   localStorage.removeItem("user_aktif");
+  localStorage.removeItem("user_id");
   notifPopup("Terima Kasih telah berkunjung Bosku!");
   Index();
 }
+
 salam();
+// function prosesLogin() {
+//   let nama = document.getElementById("username").value;
+//   if (nama === "") {
+//     notifPopup("Isi dulu usernamenya Bosku!");
+//     return false;
+//   } else {
+//     localStorage.setItem("user_aktif", nama);
+//     notifPopup("Halo " + nama + " Menghubungkan...");
+//     fade();
+//     setTimeout(() => {
+//       window.location.href = "home.html";
+//     }, 1000);
+//     return false;
+//   }
+// }
+// function salam() {
+//   let namaUser = localStorage.getItem("user_aktif");
+//   let elementSalam = document.getElementById("Salam");
+//   if (elementSalam) {
+//     if (namaUser) {
+//       elementSalam.innerText = "Halo, " + namaUser + "!";
+//     } else {
+//       notifPopup("Akses ditolak!!! Silahkan login dulu Bosku!");
+//       Index();
+//     }
+//   }
+// }
+// function prosesLogout() {
+//   localStorage.removeItem("user_aktif");
+//   notifPopup("Terima Kasih telah berkunjung Bosku!");
+//   Index();
+// }
+// salam();
